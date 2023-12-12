d30c0c9d60e080df7b64604db7c53753f2afdadb
Wsl Edge <sif7@outlook.com>
2023-12-12 01:06:12 +0000

    fcreating more distinct regex function

--- pcre-regex.c
+++ pcre-regex.c
@@ -7,40 +7,65 @@
 /* apt-get install libpcre3 libpcre3-dev */
 /* gcc pcre-regex.c -lpcre -o a.out */
 
+char **run_regex(const char *, const char *, int *);
+
 int main(int argc, char *argv[]) {
+    /* we'll start after the first quote and chop off the end quote */
+    const char *regex = "^([A-Z][a-z]+) ([A-Z][a-z]+)$";
+    // const char *subject = "Lloyd Rochester";
+    const char *subject = "John McCarthy";
+    char **rows_of_substrings = NULL;
+    int amount_of_matches = -1;
+
+    rows_of_substrings = run_regex(regex, subject, &amount_of_matches);
+
+    if (rows_of_substrings == NULL) {
+        fprintf(stderr, "rows_of_substrings = NULL\n");
+    } else {
+        if (amount_of_matches <= 0) {
+            fprintf(stderr, "amount_of_matches <= 0\n");
+        } else {
+            for (int i = 0; i < amount_of_matches; ++i) {
+                printf("%d: %s\n", i, rows_of_substrings[i]);
+                free(rows_of_substrings[i]);
+                rows_of_substrings[i] = NULL;
+            }
+            free(rows_of_substrings);
+            rows_of_substrings = NULL;
+        }
+    }
+
+    return 0;
+}
+
+char **run_regex(const char *regex, const char *subject, int *amount_of_matches) {
     /* for pcre_compile */
     pcre *re;
     const char *error;
     int erroffset;
 
     /* for pcre_exec */
-    int rc;  // count
     int ovector[30];
 
     /* to get substrings from regex */
-    int rc2 = -100;
     const char *substring;
 
-    char **rows_of_substrings;
+    char **rows_of_substrings = NULL;
 
-    /* we'll start after the first quote and chop off the end quote */
-    const char *regex = "^([A-Z][a-z]+) ([A-Z][a-z]+)$";
-    const char *subject = "Lloyd Rochester";
     // const char *subject = "John McCarthy";
     re = pcre_compile(regex, 0, &error, &erroffset, NULL);
 
-    rc = pcre_exec(re, NULL, subject, strlen(subject), 0, 0, ovector, 30);
+    *amount_of_matches = pcre_exec(re, NULL, subject, strlen(subject), 0, 0, ovector, 30);
 
-    if (rc == PCRE_ERROR_NOMATCH) {
+    if (*amount_of_matches == PCRE_ERROR_NOMATCH) {
         fprintf(stderr, "no match\n");
-    } else if (rc < -1) {
-        fprintf(stderr, "error %d from regex\n", rc);
+    } else if (*amount_of_matches < -1) {
+        fprintf(stderr, "error %d from regex\n", *amount_of_matches);
     } else {
-        rows_of_substrings = malloc(rc * sizeof(char *));
+        rows_of_substrings = malloc((*amount_of_matches) * sizeof(char *));
         /* loop through matches and return them */
-        for (int i = 0; i < rc; ++i) {
-            rc2 = pcre_get_substring(subject, ovector, rc, i, &substring);
-            printf("%d: %s\n", i, substring);
+        for (int i = 0; i < *amount_of_matches; ++i) {
+            pcre_get_substring(subject, ovector, *amount_of_matches, i, &substring);
             rows_of_substrings[i] = malloc(30 * sizeof(char));
             strncpy(rows_of_substrings[i], substring, 30);
             pcre_free_substring(substring);
@@ -48,16 +73,5 @@ int main(int argc, char *argv[]) {
     }
     pcre_free(re);
 
-    printf("\nrc=%d, rc2=%d\n", rc, rc2);
-    printf("\n");
-
-    for (int i = 0; i < rc; ++i) {
-        printf("%s\n", rows_of_substrings[i]);
-        free(rows_of_substrings[i]);
-        rows_of_substrings[i] = NULL;
-    }
-    free(rows_of_substrings);
-    rows_of_substrings=NULL;
-
-    return 0;
+    return rows_of_substrings;
 }
